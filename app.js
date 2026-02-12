c3d398af8e537869253430763df9d1f6ad3d4b78
Aditya Kumar <adityasrivastava716@gmail.com>
2026-02-12 22:00:05 +0530

    Ready to deploy on Render

--- app.js
+++ app.js
@@ -1,47 +1,53 @@
 require("dotenv").config();
 
 const express = require("express");
-const path = require('path')
-const router = require('./routes/user')
-const blogRoute = require("./routes/blog");
+const path = require("path");
 const mongoose = require("mongoose");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+
+const router = require("./routes/user");
+const blogRoute = require("./routes/blog");
 const Blog = require("./model/blog");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
 
-
-
 const app = express();
-const PORT = process.env.PORT ||3000;// here we are choosing our own port but, when we are hosting somewhere that port me not be available
 
-// //^connecting mongodb
-// mongoose.connect("mongodb://127.0.0.1:27017/blogify").then( _ => console.log('MongoDb Connected')
-// )
+//PORT FIX
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL).
-then((e)=> console.log("Db connected"))
+// ---------- Database ----------
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error(" MongoDB connection error:", err));
 
-//^ Middlewares
-app.set('view engine', 'ejs');
-app.set('views',path.resolve('./views'));
+// ---------- View Engine ----------
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.use(express.urlencoded({extended: false}));
+// ---------- Middlewares ----------
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
-app.use(express.static(path.resolve('./public')));
+app.use(express.static(path.resolve("./public")));
 
-app.get('/',async(req,res)=>{
-    console.log("User object:", req.user);
-    const allBlogs = await Blog.find().populate('createdBy').sort({createdAt: -1});
-    return res.render('home', {
-        user: req.user,
-        blogs: allBlogs,
-    });
-})
+// ---------- Routes ----------
+app.get("/", async (req, res) => {
+  const allBlogs = await Blog
+    .find()
+    .populate("createdBy")
+    .sort({ createdAt: -1 });
 
-app.use('/user',router);
-app.use('/blog', blogRoute);
+  return res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Server started at PORT: ${PORT}`);
-})
+app.use("/user", router);
+app.use("/blog", blogRoute);
+
+// ---------- Server ----------
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
